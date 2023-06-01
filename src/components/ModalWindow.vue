b55d3ddd19bd65ff93b1ec62b26049e87019bd03
Nikolai <50028968+Brodatsky@users.noreply.github.com>
2023-06-01 22:53:06 +0300

    modal

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="modal" v-if="show" @click="closeModal">
+    <div class="content"><slot> </slot></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  show: boolean
+}>()
+
+const emit = defineEmits(['update:show'])
+
+function closeModal() {
+  emit('update:show', false)
+}
+</script>
+
+<style scoped>
+.modal {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+.content {
+  margin: auto;
+  background: rgb(255, 255, 255);
+  border-radius: 12px;
+  min-height: 100px;
+  min-width: 300px;
+  padding: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,28 @@
         <ProductCard v-if="product.id" :product="product" />
       </template>
     </div>
+    <button
+      class="transition ease-in-out duration-300 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 border w-full rounded-full p-1"
+      @click="showModal"
+    >
+      New Product
+    </button>
+    <ModalWindow v-model:show="modalVisability">123</ModalWindow>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useProductStore } from '@/store/useProductStore'
 import ProductCard from '@/components/ProductCard.vue'
+import ModalWindow from '@/components/ModalWindow.vue'
 
 const productStore = useProductStore()
+
+let modalVisability = ref(false)
+function showModal() {
+  modalVisability.value = true
+}
 </script>
 
 <style scoped></style>
