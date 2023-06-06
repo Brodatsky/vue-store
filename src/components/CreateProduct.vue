7c282b89527f0c489608a65d61ca84478c2a542b
Nikolai <50028968+Brodatsky@users.noreply.github.com>
2023-06-06 23:05:57 +0300

    validate

--- src/components/CreateProduct.vue
+++ src/components/CreateProduct.vue
@@ -1,89 +1,52 @@
 <template>
   <div class="modal">
     <h1 class="font-bold text-center">Create new product</h1>
-    <form action="">
-      <div class="flex flex-col items-center">
-        <input v-model="title" class="input mb-3" placeholder="Type title" type="text" required />
-        <input
-          v-model="price"
-          class="input mb-3"
-          placeholder="Type price"
-          type="number"
-          required="true"
-        />
-        <input
-          v-model="description"
-          class="input mb-3"
-          placeholder="Type description"
-          type="text"
-          required
-        />
-        <input
-          v-model="category"
-          class="input mb-5"
-          placeholder="Type category"
-          type="text"
-          required
-        />
-        <button
-          type="submit"
-          class="transition ease-in-out duration-300 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 border w-64 rounded-full p-1"
-          @click="submitNewProduct"
-        >
-          Create new product
-        </button>
-      </div>
-    </form>
+
+    <Form @submit="onSubmit" :validation-schema="schema">
+      <label for="title">Title</label>
+      <Field name="title" class="input mb-5" />
+      <ErrorMessage name="title" class="block mb-5" />
+      <label for="price">Price</label>
+      <Field name="price" type="number" modelValue="10" class="input mb-5" />
+      <ErrorMessage name="price" class="block mb-5" />
+      <label for="description">Description</label>
+      <Field name="description" class="input mb-5" />
+      <ErrorMessage name="description" class="block mb-5" />
+      <label for="category">Category</label>
+      <Field name="category" class="input mb-5" />
+      <ErrorMessage name="category" class="block mb-5" />
+      <button
+        class="transition ease-in-out duration-300 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 border w-64 rounded-full p-1"
+      >
+        Submit
+      </button>
+    </Form>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import axios from 'axios'
 
 import { useProductStore } from '@/store/useProductStore'
 import type { Product } from '@/store/useProductStore'
 
+import { Field, Form, ErrorMessage } from 'vee-validate'
+import * as yup from 'yup'
+
 const productStore = useProductStore()
 
-let title = ref<string | null>(null)
-let price = ref<number | null>(null)
-let description = ref<string | null>(null)
-let category = ref<string | null>(null)
+const schema = yup.object({
+  title: yup.string().required(),
+  price: yup.number().required(),
+  description: yup.string().required(),
+  category: yup.string().required()
+})
 
-function checkForm() {
-  if (title.value && price.value && description.value && category.value) {
-    return true
-  }
-
-  const errors = []
-
-  if (title.value) {
-    errors.push('Требуется указать имя.')
-  }
-  if (price.value) {
-    errors.push('Требуется указать цену.')
-  }
-  return false
-}
-
-async function submitNewProduct() {
-  if (!checkForm()) {
-    console.log(checkForm())
-
-    return false
-  }
-  let productData: Product = {
-    title: title.value as string,
-    price: price.value as number,
-    description: description.value as string,
-    image: `https://placehold.co/200x300?text=ss` as string,
-    category: category.value as string
-  }
-  console.log(123)
+async function onSubmit(product: any, actions: any) {
+  product.image = `https://placehold.co/400x600?text=${product.title}`
 
   try {
-    const res = await axios.post<Product>('https://fakestoreapi.com/products', productData)
+    const res = await axios.post<Product>('https://fakestoreapi.com/products', product)
     productStore.addProduct(res.data)
   } catch (error) {
     console.log(error)
